c6e59fea6a587a44a630aac74fe8c6a5658748e3
Ilija Stanojević <stanojevic.sv71.2020@uns.ac.rs>
2023-05-29 14:39:27 +0200

    Add model

---
+++ HCI-Tim-15-2023/Model/Accomodation.cs
@@ -0,0 +1,16 @@
+﻿namespace HCI_Tim_15_2023.Model;
+
+public class Accomodation : Location
+{
+    public Accomodation()
+    {
+    }
+    public Accomodation(string id, double lon, double lat, string address, string name)
+    {
+        this.id = id;
+        this.lon = lon;
+        this.lat = lat;
+        this.address = address;
+        this.name = name;
+    }
+}
---
+++ HCI-Tim-15-2023/Model/Location.cs
@@ -0,0 +1,15 @@
+﻿using ZstdSharp.Unsafe;
+
+namespace HCI_Tim_15_2023.Model;
+
+public class Location
+{
+
+
+    public string id { get; set; }
+    public double lon { get; set; }
+    public double lat { get; set; }
+    public string address { get; set; }
+    public string name { get; set; }
+
+}
