0426590890164fa6ddf84db9afa395df7c384778
REX1389 <rrvlelcc@gmail.com>
2023-06-07 21:23:15 +0200

    Added travel view pages

---
+++ HCI-Tim-15-2023/GUI/Pregledi/AgentMonthlySoldTravelViewPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace HCI_Tim_15_2023.GUI.Pregledi;
+
+public partial class AgentMonthlySoldTravelViewPage : Page
+{
+    public AgentMonthlySoldTravelViewPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ HCI-Tim-15-2023/GUI/Pregledi/ClientBoughtTravelViewPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace HCI_Tim_15_2023.GUI.Pregledi;
+
+public partial class ClientBoughtTravelViewPage : Page
+{
+    public ClientBoughtTravelViewPage()
+    {
+        InitializeComponent();
+    }
+}
