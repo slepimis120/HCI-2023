2a2b441b9822e0efad1443ebe38453bccc6adc3b
Ilija Stanojević <stanojevic.sv71.2020@uns.ac.rs>
2023-06-08 00:16:48 +0200

    Add searching for restaurants

--- HCI-Tim-15-2023/GUI/RestaurantCrudPage.xaml.cs
+++ HCI-Tim-15-2023/GUI/RestaurantCrudPage.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MongoDB.Bson;
 
 namespace HCI_Tim_15_2023.GUI;
 public partial class RestaurantCrudPage : Page
@@ -25,6 +26,27 @@ public partial class RestaurantCrudPage : Page
         restaurantsDataGrid.ItemsSource = restaurants;
     }
 
+    private List<Restaurant> GetFilteredRestaurantsFromDB(string searchTerm)
+    {
+        string connectionString = "mongodb://localhost:27017";
+        string databaseName = "hci";
+        string collectionName = "restaurants";
+
+        var client = new MongoClient(connectionString);
+
+        var database = client.GetDatabase(databaseName);
+        var collection = database.GetCollection<Restaurant>(collectionName);
+
+        var filterBuilder = Builders<Restaurant>.Filter;
+        var filter = filterBuilder.Or(
+            filterBuilder.Regex(r => r.name, new BsonRegularExpression(searchTerm, "i")),
+            filterBuilder.Regex(r => r.address, new BsonRegularExpression(searchTerm, "i"))
+        );
+
+        var filteredRestaurants = collection.Find(filter).ToList();
+
+        return filteredRestaurants;
+    }
     private List<Restaurant> GetRestaurantsFromDB()
     {
         string connectionString = "mongodb://localhost:27017";
@@ -39,5 +61,18 @@ public partial class RestaurantCrudPage : Page
         var restaurants = collection.Find(filter).ToList();
 
         return restaurants;
+    }
+
+    private void AddButton_Click(object sender, RoutedEventArgs e)
+    {
+    }
+
+    private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        string searchTerm = searchTextBox.Text;
+
+        var filteredRestaurants = GetFilteredRestaurantsFromDB(searchTerm);
+
+        restaurantsDataGrid.ItemsSource = filteredRestaurants;
     }
 }
