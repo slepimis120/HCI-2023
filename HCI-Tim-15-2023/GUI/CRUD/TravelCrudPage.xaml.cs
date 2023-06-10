d887156b8a73382a87dfbeb44c614eb824b3daee
Ilija Stanojević <stanojevic.sv71.2020@uns.ac.rs>
2023-06-10 20:49:01 +0200

    Add crud page for travels

---
+++ HCI-Tim-15-2023/GUI/CRUD/TravelCrudPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace HCI_Tim_15_2023.GUI.CRUD;
+
+public partial class TravelCrudPage : Page
+{
+    public TravelCrudPage()
+    {
+        InitializeComponent();
+    }
+}
