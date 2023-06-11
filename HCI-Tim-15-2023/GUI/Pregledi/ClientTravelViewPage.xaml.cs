4c74d127cb2456f638430029986054366942f7c2
REX1389 <rrvlelcc@gmail.com>
2023-06-11 16:09:04 +0200

    ListBoxItem style

--- HCI-Tim-15-2023/GUI/Pregledi/ClientTravelViewPage.xaml.cs
+++ HCI-Tim-15-2023/GUI/Pregledi/ClientTravelViewPage.xaml.cs
@@ -1,11 +1,30 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace HCI_Tim_15_2023.GUI.Pregledi;
 
 public partial class ClientTravelViewPage : Page
 {
+    ListBoxItem selectedTravel = null;
+
     public ClientTravelViewPage()
     {
         InitializeComponent();
+    }
+
+    private void Travel_Selected(object sender, System.Windows.RoutedEventArgs e)
+    {
+        if(selectedTravel != null)
+        {
+            selectedTravel.BorderThickness = new Thickness(2);
+            selectedTravel.BorderBrush = Brushes.Blue;
+            selectedTravel.SetValue(BackgroundProperty, Brushes.LightSkyBlue);
+        }
+        selectedTravel = sender as ListBoxItem;
+        selectedTravel.BorderBrush = Brushes.DarkOrange;
+        selectedTravel.BorderThickness = new Thickness(4);
+        selectedTravel.SetValue(BackgroundProperty, Brushes.Orange);
+        selectedTravel.IsSelected = false;
     }
 }
