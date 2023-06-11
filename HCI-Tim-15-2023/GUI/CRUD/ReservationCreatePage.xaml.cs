d13d8c3573054192844ba3a6387b3e34a186e63c
unknown <slepimis120@gmail.com>
2023-06-12 00:34:58 +0200

    Small fixes in design and function of the login/register

---
+++ HCI-Tim-15-2023/GUI/CRUD/ReservationCreatePage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HCI_Tim_15_2023.GUI.CRUD
+{
+    /// <summary>
+    /// Interaction logic for ReservationCreatePage.xaml
+    /// </summary>
+    public partial class ReservationCreatePage : Page
+    {
+        public ReservationCreatePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- HCI-Tim-15-2023/GUI/ClientHomePage.xaml.cs
+++ HCI-Tim-15-2023/GUI/ClientHomePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using HCI_Tim_15_2023.GUI.Pregledi;
+﻿using HCI_Tim_15_2023.GUI.CRUD;
+using HCI_Tim_15_2023.GUI.Pregledi;
 using HCI_Tim_15_2023.Model;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,6 +23,11 @@ public partial class ClientHomePage : Page
     private void OpenClientBoughtTravelViewPage(object sender, RoutedEventArgs e)
     {
         this.NavigationService.Navigate(new ClientBoughtTravelViewPage());
+    }
+
+    private void OpenClientReservationCreatePage(object sender, RoutedEventArgs e)
+    {
+        this.NavigationService.Navigate(new ReservationCreatePage());
     }
 
     private void LogOut(object sender, RoutedEventArgs e)
