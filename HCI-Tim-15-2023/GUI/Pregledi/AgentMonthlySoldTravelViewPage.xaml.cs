7d3d21b515b965e608fe6d8d8c19fb63d5e179ee
REX1389 <rrvlelcc@gmail.com>
2023-06-13 09:35:08 +0200

    Monthly sold travels

--- HCI-Tim-15-2023/GUI/Pregledi/AgentMonthlySoldTravelViewPage.xaml.cs
+++ HCI-Tim-15-2023/GUI/Pregledi/AgentMonthlySoldTravelViewPage.xaml.cs
@@ -260,4 +260,76 @@ public partial class AgentMonthlySoldTravelViewPage : Page
 
         LoadTravels();
     }
+
+    private void Left(object sender, RoutedEventArgs e)
+    {
+        month--;
+        if(month < 1)
+        {
+            month = 12;
+            year--;
+        }
+
+        UpdateMonth();
+    }
+
+    private void Right(object sender, RoutedEventArgs e)
+    {
+        month++;
+        if (month > 12)
+        {
+            month = 1;
+            year++;
+        }
+
+        UpdateMonth();
+    }
+
+    private void UpdateMonth()
+    {
+        string text = "";
+        switch(month)
+        {
+            case 1:
+                text = "January";
+                break;
+            case 2:
+                text = "February";
+                break;
+            case 3:
+                text = "March";
+                break;
+            case 4:
+                text = "April";
+                break;
+            case 5:
+                text = "May";
+                break;
+            case 6:
+                text = "June";
+                break;
+            case 7:
+                text = "July";
+                break;
+            case 8:
+                text = "August";
+                break;
+            case 9:
+                text = "September";
+                break;
+            case 10:
+                text = "October";
+                break;
+            case 11:
+                text = "November";
+                break;
+            case 12:
+                text = "December";
+                break;
+        }
+        text += " " + year;
+
+        btnMonth.Content = text;
+        LoadTravels();
+    }
 }
